fa8b042d6c4b110f4d6c81f3d2a198d9d41f9032
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-17 19:16:02 +0300

    initial commit

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
-
+  useEffect(() => {
+   
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => {
+        if (prevTime === 1) {
+        
+          onAnswered(false);
+          return 10; 
+        }
+        return prevTime - 1; 
+      });
+    }, 1000); 
+   
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]); 
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
+    setTimeRemaining(10); 
+    onAnswered(isCorrect); 
   }
 
   const { id, prompt, answers, correctIndex } = question;
